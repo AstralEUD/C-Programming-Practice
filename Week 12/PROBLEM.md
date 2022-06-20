2f3638c0f9c752e7d86b136600b05cad8d62d007
Testify <eud28280@gmail.com>
2022-06-20 23:06:02 +0900

    Week 12

---
+++ Week 12/PROBLEM.md
@@ -0,0 +1,6 @@
+# C Programming (Practice) I
+**Dept of Computer Education, C Programming (Practice) I, Week 12**
+
+ - n명의 국어, 수학, 영어 성적 입력 후 각 과목별 평균, 학생 별 평균 계산 후 table 형태로 출력 (함수 사용)
+ - 버블 정렬 (n개의 case 입력)
+
---
+++ Week 12/p2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define SIZE 5
+
+int main (void) {
+	int i,k, tmp, n, t;
+	int list[100] = {0};
+	//int list[100] = {16,7,9,1,3};
+	printf("�󸶳� ���� �� �Է�? : ");
+	scanf("%d",&n); 
+	for (t=0; t<n; t++) {
+		scanf("%d",&list[t]);
+	}
+	for (k=0; k<=n-1; k++) {
+		for (i=0; i <n-1; i++) {
+			if (list[i] > list[i+1]) {
+				tmp = list [i];
+				list[i] = list[i + 1];
+				list[i+1] = tmp;
+			}
+		}
+	}
+		for (i=0; i<=n-1; i++) {
+			printf("%d ",list[i]);
+		}
+}
