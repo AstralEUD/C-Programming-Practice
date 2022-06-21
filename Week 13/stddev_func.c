afbfa1604574df206964ea24eaea8b1566cab8a1
Testify <eud28280@gmail.com>
2022-06-22 00:03:29 +0900

    Week 13 Sol (2)

---
+++ Week 13/stddev_func.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+int casenum = 0;
+int list[100] = {};
+int average () {
+	int i, a = 0;
+	float r;
+	for (i=0; i<wjdcasenum; i++) {
+		scanf("%d",&list[i]);
+		a+=list[i];
+	}
+	r = a/casenum;
+	return r;
+}
+int devi(int q) {
+	int i;
+	float b =0;
+	for (i=0; i<casenum; i++) {
+		b+= (list[i] - q) * (list[i] - q);
+	}
+	b = b / casenum;
+	b = sqrt(b);
+	return b;
+}
+int main () {
+	float q, p, t;
+	printf(" How many case ? : ");
+	scanf("%d",&casenum);
+	q = average();
+	printf("average : %f",q);
+	p = devi(q);
+	printf("standard deviataion (ǥ������) : %f",p);
+}
