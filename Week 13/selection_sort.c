29ede2a0d36e6ab2684fbe73abf85cf71747818f
Testify <eud28280@gmail.com>
2022-06-21 23:00:16 +0900

    Week 13 Sol. (1)

---
+++ Week 13/selection_sort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int list[100] = {0};
+int casenum = 0;
+void fn_input() {
+	int i;
+	for (i=0; i<casenum; i++) {
+		printf("%d ��° case �Է� : ",i+1);
+		scanf("%d",&list[i]);
+	}
+}
+int selection_sort () {
+	int i, q, t, p;
+	for (t=0; t<casenum; t++) {
+		q = list[t];
+		p=t;
+		for (i=t+1; i<casenum; i++) {
+			if (q > list[i]) {
+				q = list[i];
+				p=i;
+			}
+		}
+		list[p] = list[t];
+		list[t] = q;
+		fn_print(t+1);
+	}
+}
+int fn_print(int w) {
+	int i;
+	printf("\n (%d) LIST : ",w);
+	for (i=0; i<casenum; i++) {
+		printf("%d, ",list[i]);
+	}
+}
+int main () {
+	printf(" How many case? ");
+	scanf("%d",&casenum);
+	fn_input();
+	fn_print(0);
+	selection_sort();
+}
